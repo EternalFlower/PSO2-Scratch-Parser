14150513ed5cfe1fbaa82d9b0d125bd49270018c
Jimmy <jshen35@gmail.com>
2021-05-15 22:21:34 -0400

    Add project files.

---
+++ ScratchParser/ScratchData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using HtmlAgilityPack;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ScratchParser
+{
+    public class ScratchData
+    {
+
+        ScratchData()
+        {
+
+        }
+        
+        public void parseURL(string url)
+        {
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(url);
+        }
+
+        public void parseHTML(string filename)
+        {
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.Load(filename);
+        }
+    }
+}
